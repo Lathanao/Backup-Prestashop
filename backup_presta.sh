47a142a68ef1bc2220f320b8ab3700030c8cea93
Lathanao <tanguy.salmon@gmail.com>
2020-07-21 23:59:06 +0700

    Mod how get value

--- backup_presta.sh
+++ backup_presta.sh
@@ -1,57 +1,77 @@
 #!/bin/bash
 
-FILE="./app/config/parameters.php" 
+function printLogo {
+  echo -e "
+   ___                _              _          _ __
+  | _ \ _ _  ___  ___| |_  __ _  ___| |_   ___ | '_ \\
+  |  _/| '_|/ -_)(_-/|  _|/ _\` |(_-/|   \ / _ \| .__/
+  |_|  |_|  \___|/__/ \__|\__/_|/__/|_||_|\___/|_|
+  "
+}
 
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else 
-    echo "$FILE does not exist."
-    set -e
-fi
+function regexAttributeValue {
+  echo "$1" | grep -oE '[^ ]+$' | sed "s/[^[:alnum:]-]//g"
+}
 
-find ./ -name '*cart_default*' -delete
-find ./ -name '*small_default*' -delete	
-find ./ -name '*medium_default*' -delete	
-find ./ -name '*home_default*' -delete	
-find ./ -name '*large_default*' -delete	
-find ./ -name '*category_default*' -delete	
-find ./ -name '*stores_default*' -delete	
-find ./ -name '*side_default*' -delete
-find ./ --name '*LICENSE*' -delete
+function isConfFileExit {
+  if [ -f "$1" ]; then
+      echo "$1 exists."
+  else
+      echo "$1 does not exist."
+      set -e
+  fi
+}
 
-yes | rm -r ./install*
-yes | rm -r ./app/cache/*
-yes | rm -r ./app/test/*
-yes | rm -r ./var/cache/*
-yes | rm -r ./vendor/*
-yes | rm -r ./**/vendor/*
+FILE="./app/config/parameters.php"
 
-find ./themes/**/assets/cache -type f -name '*.css' -delete
-find ./themes/**/assets/cache -type f -name '*.js' -delete
 
-while read line; do
-	if echo "$line" | grep "database_host"
-	then
-		IFS='=' read -ra ADDR <<< "$line"
-		DATABASE_HOST=${ADDR[1]//[,\'\ >]}
-	fi
-	if echo "$line" | grep "database_name"
-	then
-		IFS='=' read -ra ADDR <<< "$line"
-		DATABASE_NAME=${ADDR[1]//[,\'\ >]}
-	fi
-	if echo "$line" | grep "database_user"
-	then
-		IFS='=' read -ra ADDR <<< "$line"
-		DATABASE_USER=${ADDR[1]//[,\'\ >]}
-	fi
-	if echo "$line" | grep "database_password"
-	then
-		IFS='=' read -ra ADDR <<< "$line"
-		DATABASE_PASSWORD=${ADDR[1]//[,\'\ >]}
-	fi
+function removeGeneratedPictures {
+  find ./ -name '*cart_default*' -delete
+  find ./ -name '*small_default*' -delete
+  find ./ -name '*medium_default*' -delete
+  find ./ -name '*home_default*' -delete
+  find ./ -name '*large_default*' -delete
+  find ./ -name '*category_default*' -delete
+  find ./ -name '*stores_default*' -delete
+  find ./ -name '*side_default*' -delete
+  find ./ --name '*LICENSE*' -delete
+}
+
+function removeCachedDirectories {
+  yes | rm -r ./install*
+  yes | rm -r ./app/cache/*
+  yes | rm -r ./app/test/*
+  yes | rm -r ./var/cache/*
+  yes | rm -r ./vendor/*
+  yes | rm -r ./**/vendor/*
+}
+
+function deleteCompiledAsstes {
+  find ./themes/**/assets/cache -type f -name '*.css' -delete
+  find ./themes/**/assets/cache -type f -name '*.js' -delete
+}
+printLogo
+isConfFileExit ${FILE}
+#removeGeneratedPictures
+#deleteCompiledAsstes
+
+while read LINE; do
+  case $(echo ${LINE} | grep -Eo '^[^ ]+' ) in
+    \'database_host\') DATABASE_HOST=$(regexAttributeValue "${LINE}") ;;
+    \'database_name\') DATABASE_NAME=$(regexAttributeValue "${LINE}") ;;
+    \'database_user\') DATABASE_USER=$(regexAttributeValue "${LINE}") ;;
+    \'database_password\') DATABASE_PASSWORD=$(regexAttributeValue "${LINE}") ;;
+  esac
 done < $FILE
 
-mysqldump -u $DATABASE_USER -p"${DATABASE_PASSWORD}" -h $DATABASE_HOST $DATABASE_NAME > $(date +%F)_$DATABASE_NAME.sql
+echo "------------"
+echo $DATABASE_USER
+echo $DATABASE_NAME
+echo $DATABASE_USER
+echo $DATABASE_PASSWORD
 
-zip -r /tmp/`date +%Y%m%d`_${PWD##*/}.zip ./ --exclude "*.git*" --exclude "*cache*"
+#mysqldump -u $DATABASE_USER -p"${DATABASE_PASSWORD}" -h $DATABASE_HOST $DATABASE_NAME > $(date +%F)_$DATABASE_NAME.sql
+#
+#zip -r /tmp/`date +%Y%m%d`_${PWD##*/}.zip ./ --exclude "*.git*" --exclude "*cache*"
+
+
