a6dab80f6b1195744dc725465e64ea07dd0dd570
Lathanao <tanguy.salmon@gmail.com>
2020-07-28 17:55:25 +0700

    Add stuff / improve code

--- backup_presta.sh
+++ backup_presta.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 function printLogo {
   echo -e "
-   ___                _              _          _ __
-  | _ \ _ _  ___  ___| |_  __ _  ___| |_   ___ | '_ \\
-  |  _/| '_|/ -_)(_-/|  _|/ _\` |(_-/|   \ / _ \| .__/
-  |_|  |_|  \___|/__/ \__|\__/_|/__/|_||_|\___/|_|
+  \e[38;5;60m ___                _          \e[38;5;196m    _          _ __
+  \e[38;5;61m| _ \ _ _  ___  ___| |_  __ _  \e[38;5;197m___| |_   ___ | '_ \\
+  \e[38;5;62m|  _/| '_|/ -_)(_-/|  _|/ _\` |\e[38;5;198m(_-/|   \ / _ \| .__/
+  \e[38;5;63m|_|  |_|  \___|/__/ \__|\__/_|\e[38;5;199m/__/|_||_|\___/|_|
   "
-}
-
-function regexAttributeValue {
-  echo "$1" | grep -oE '[^ ]+$' | sed "s/[^[:alnum:]-]//g"
 }
 
 function isConfFileExit {
@@ -22,10 +18,10 @@ function isConfFileExit {
   fi
 }
 
-FILE="./app/config/parameters.php"
-
-
-function removeGeneratedPictures {
+function removeGeneratedPictures() {
+  printf "##-----------------------------------------------------\n"
+  printf "## Remove Generated Pictures\n"
+  printf "##-----------------------------------------------------\n"
   find ./ -name '*cart_default*' -delete
   find ./ -name '*small_default*' -delete
   find ./ -name '*medium_default*' -delete
@@ -34,106 +30,96 @@ function removeGeneratedPictures {
   find ./ -name '*category_default*' -delete
   find ./ -name '*stores_default*' -delete
   find ./ -name '*side_default*' -delete
-  find ./ --name '*LICENSE*' -delete
 }
 
-function removeCachedDirectories {
-  yes | rm -r ./install*
-  yes | rm -r ./app/cache/*
-  yes | rm -r ./app/test/*
-  yes | rm -r ./var/cache/*
-  yes | rm -r ./vendor/*
-  yes | rm -r ./**/vendor/*
+function removeCachedDirectories() {
+  printf "##-----------------------------------------------------\n"
+  printf "## Remove Cached Directories\n"
+  printf "##-----------------------------------------------------\n"
+  rm -rf ./install*
+  rm -rf ./app/cache/*
+  rm -rf ./app/test/*
+  rm -rf ./var/cache/*
 }
 
-function deleteCompiledAsstes {
+function removeComposerDirectories() {
+  printf "##-----------------------------------------------------\n"
+  printf "## Remove Composer Directories\n"
+  printf "##-----------------------------------------------------\n"
+  rm -rf ./vendor/*
+  rm -rf ./**/vendor/*
+}
+
+function removeCompiledAssets() {
+  printf "##-----------------------------------------------------\n"
+  printf "## Remove Compiled Assets\n"
+  printf "##-----------------------------------------------------\n"
   find ./themes/**/assets/cache -type f -name '*.css' -delete
   find ./themes/**/assets/cache -type f -name '*.js' -delete
 }
+
+function removeNodeDirectories() {
+  find . -type d -name 'node_modules' -exec rm -r {} +
+}
+
+function extractCredentials() {
+
+  DATABASE_HOST=$(awk '/database_host/ {print $3}' $FILE | sed "s/[^[:alnum:]-]//g")
+  DATABASE_NAME=$(awk '/database_name/ {print $3}' $FILE | sed "s/[^[:alnum:]-]//g")
+  DATABASE_USER=$(awk '/database_user/ {print $3}' $FILE | sed "s/[^[:alnum:]-]//g")
+  DATABASE_PASSWORD=$(awk '/database_password/ {print $3}' $FILE | sed "s/[^[:alnum:]-]//g")
+}
+
+function printCredentials() {
+  echo "-----CREDENTIAL-------"
+  echo $DATABASE_USER
+  echo $DATABASE_NAME
+  echo $DATABASE_USER
+  echo $DATABASE_PASSWORD
+}
+
+
+function createDumpDB() {
+  mysqldump -u $DATABASE_USER -p"${DATABASE_PASSWORD}" -h $DATABASE_HOST $DATABASE_NAME > $(date +%F)_$DATABASE_NAME.sql
+}
+
+function createZipArchive() {
+  zip -r /tmp/`date +%Y%m%d`_${PWD##*/}.zip ./ --exclude "*.git*" --exclude "*cache*"
+}
+
+
 printLogo
+FILE="./app/config/parameters.php"
 isConfFileExit ${FILE}
-#removeGeneratedPictures
-#deleteCompiledAsstes
 
-while read LINE; do
-  case $(echo ${LINE} | grep -Eo '^[^ ]+' ) in
-    \'database_host\') DATABASE_HOST=$(regexAttributeValue "${LINE}") ;;
-    \'database_name\') DATABASE_NAME=$(regexAttributeValue "${LINE}") ;;
-    \'database_user\') DATABASE_USER=$(regexAttributeValue "${LINE}") ;;
-    \'database_password\') DATABASE_PASSWORD=$(regexAttributeValue "${LINE}") ;;
+options=(
+'Make a SQL dump in root project'
+'Make a zip archive'
+'Remove all cached + compiled files'
+'Remove all generated pictures'
+'Remove all Node.js files'
+'Remove all Composer files'
+'( 3 -> 6 ) + 1 + 2'
+'About'
+'Exit'
+)
+
+select menu in "${options[@]}";
+do
+  echo "##-----------------------------------------------------\n"
+  echo "## $menu ($REPLY)\n"
+  echo "##-----------------------------------------------------\n"
+
+  case $REPLY in
+    1) createDumpDB;;
+    2) createZipArchive;;
+    2) removeCachedDirectories;;
+    2) removeGeneratedPictures;;
+    2) removeNodeDirectories;;
+    2) removeComposerDirectories;;
+    2) createDumpDB;createZipArchive;removeCachedDirectories;removeGeneratedPictures;removeNodeDirectories;removeComposerDirectories;createDumpDB;createZipArchive;;
+    2) echo 'Please check README';;
+    2) exit 0;;
   esac
-done < $FILE
 
-echo "------------"
-echo $DATABASE_USER
-echo $DATABASE_NAME
-echo $DATABASE_USER
-echo $DATABASE_PASSWORD
-
-#mysqldump -u $DATABASE_USER -p"${DATABASE_PASSWORD}" -h $DATABASE_HOST $DATABASE_NAME > $(date +%F)_$DATABASE_NAME.sql
-#
-#zip -r /tmp/`date +%Y%m%d`_${PWD##*/}.zip ./ --exclude "*.git*" --exclude "*cache*"
-
-
-E='echo -e';
-e='echo -en';
-trap "R;exit" 2
-    ESC=$( $e "\e")
-   TPUT(){ $e "\e[${1};${2}H";}
-  CLEAR(){ $e "\ec";}
-  CIVIS(){ $e "\e[?45l";}
-   DRAW(){ $e "\e%@\e(0";}
-  WRITE(){ $e "\e(B";}
-   MARK(){ $e "\e[7m";}
- UNMARK(){ $e "\e[27m";}
-      R(){ stty sane;$e "\e[37;210m\e[J";};
-   HEAD(){ DRAW
-           for each in $(seq 1 13);do
-           $E "   x                                          x"
-           done
-           WRITE;MARK;TPUT 1 5
-           $E "BASH SELECTION MENU";UNMARK;}
-           i=0; CIVIS;NULL=/dev/null
-   FOOT(){ MARK;TPUT 13 5
-           printf "ENTER - SELECT,NEXT";UNMARK;}
-  ARROW(){ read -s -n3 key 2>/dev/null >&2
-           if [[ $key = $ESC[A ]];then echo up;fi
-           if [[ $key = $ESC[B ]];then echo dn;fi;}
-     M0(){ TPUT  3 2; $e "[*] Make a SQL dump in root project";}
-     M1(){ TPUT  4 2; $e "[*] Make a zip archive";}
-     M2(){ TPUT  5 2; $e "[*] Remove all cached + compiled files";}
-     M3(){ TPUT  6 2; $e "[*] Remove all generated pictures";}
-     M4(){ TPUT  7 2; $e "[*] Remove all Node.js files";}
-     M5(){ TPUT  8 2; $e "[*] Remove all Composer files";}
-     M6(){ TPUT  9 2; $e "[*] ( 3 -> 6 ) + 1 + 2 ";}
-     M7(){ TPUT  10 2; $e "[*] About  ";}
-     M8(){ TPUT  11 2; $e "[*] Exit   ";}
-      LM=8
-   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
-    POS(){ if [[ $cur == up ]];then ((i--));fi
-           if [[ $cur == dn ]];then ((i++));fi
-           if [[ $i -lt 0   ]];then i=$LM;fi
-           if [[ $i -gt $LM ]];then i=0;fi;}
-REFRESH(){ after=$((i+1)); before=$((i-1))
-           if [[ $before -lt 0  ]];then before=$LM;fi
-           if [[ $after -gt $LM ]];then after=0;fi
-           if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
-           if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
-           M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
-   INIT(){ MENU;}
-     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
-     ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
-  while [[ "$O" != " " ]]; do
-  case $i in
-        0) S=M0;SC;if [[ $cur == "" ]];then R;$e "\n$(w        )\n";ES;fi;;
-        1) S=M1;SC;if [[ $cur == "" ]];then R;$e "\n$(ifconfig )\n";ES;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;$e "\n$(removeCachedDirectories)\n";ES;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;$e "\n$(route -n )\n";ES;fi;;
-        4) S=M4;SC;if [[ $cur == "" ]];then R;$e "\n$(date     )\n";ES;fi;;
-        5) S=M5;SC;if [[ $cur == "" ]];then R;$e "\n$($e by oTo)\n";ES;fi;;
-        6) S=M6;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
-        7) S=M7;SC;if [[ $cur == "" ]];then R;$e "\n$($e by oTo)\n";ES;fi;;
-        8) S=M8;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
-  esac;POS;done
-
-set -e
+done
